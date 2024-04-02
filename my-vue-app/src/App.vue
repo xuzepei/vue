88229b12c29e075af2b1d8e33b0612de873b94ec
xuzepei <xuzepei@gmail.com>
2024-04-02 18:07:14 +0800

    no message

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <MyHelloWorld/>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import MyHelloWorld from './components/MyHelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyHelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-vue-app/src/components/MyHelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+        <button @click="changeMsg">改变消息</button>
+    </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: "Hello, Vue!"
+        };
+    },
+    methods: {
+        changeMsg() {
+            this.msg = "Hello, World!";
+        }
+    }
+};
+</script>
