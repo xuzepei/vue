1007dae90377503971285df7bfa8b8188f3e17bb
xuzepei <xuzepei@gmail.com>
2024-09-09 21:19:39 +0800

    no message

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,36 +1,43 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" width="100" height="100"> -->
+  <MyHelloWorld :msgInProps="greeting" @response="fromChildResponse" />
+  <div> {{ childMsg || 'No child msg yet' }}</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import MyHelloWorld from './components/MyHelloWorld.vue'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    MyHelloWorld,
+  },
+  data() { 
+    return {
+      greeting: "Hello from parent.",
+      childMsg: null,
+    }
+  },
+  methods: {
+    fromChildResponse(msg) { 
+      console.log("fromChildResponse: " + msg)
+      this.childMsg = msg
+    },
+  },
+  mounted() { 
+    console.log('App has been mounted!');
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: black;
   margin-top: 60px;
 }
 </style>
