5dc95bdd7edc451503ecc08fe18236e80409c47f
xuzepei <xuzepei@gmail.com>
2024-09-17 22:52:50 +0800

    表单数据绑定和简单验证

--- my-vue2-project/src/components/Login.vue
+++ my-vue2-project/src/components/Login.vue
@@ -6,19 +6,21 @@
                 <img src="../assets/logo.png" alt="">
             </div>
             <!-- 登录表单 -->
-            <el-form class="login_form" label-width="0px">
+            <el-form ref="loginFormRef" class="login_form" label-width="0px" :model="loginForm" :rules="loginFormRules">
                 <!-- 用户名 -->
-                <el-form-item label="">
-                    <el-input prefix-icon="iconfont icon-yonghu" placeholder="请输入用户名"></el-input>
+                <el-form-item prop="username">
+                    <el-input v-model="loginForm.username" prefix-icon="iconfont icon-yonghu"
+                        placeholder="请输入用户名"></el-input>
                 </el-form-item>
                 <!-- 密码 -->
-                <el-form-item label="">
-                    <el-input prefix-icon="iconfont icon-mima" placeholder="请输入密码"></el-input>
+                <el-form-item prop="password">
+                    <el-input v-model="loginForm.password" prefix-icon="iconfont icon-mima" placeholder="请输入密码"
+                        type="password"></el-input>
                 </el-form-item>
                 <!-- 按钮区 -->
                 <el-form-item label="" class="buttons">
                     <el-button type="primary">登录</el-button>
-                    <el-button>重置</el-button>
+                    <el-button @click="resetLoginForm">重置</el-button>
                 </el-form-item>
             </el-form>
         </div>
@@ -81,7 +83,6 @@
     display: flex;
     justify-content: flex-end;
 }
-
 </style>
 
 <!-- 非 scoped 的样式，全局生效 -->
@@ -89,11 +90,37 @@
 .icon-yonghu {
     margin-left: 3px;
 }
+
 .icon-mima {
     margin-left: 3px;
 }
 </style>
 
 <script>
-export default {}
+export default {
+    data() {
+        return {
+            //登录表单数据绑定对象
+            loginForm: {
+                username: "",
+                password: "",
+            },
+            loginFormRules: {
+                username: [
+                    { required: true, message: '请输入登录用户名', trigger: 'blur' },
+                    { min: 3, max: 10, message: "长度在3到10个字符", trigger: 'blur' },
+                ],
+                password: [
+                    { required: true, message: '请输入登录密码', trigger: 'blur' },
+                    { min: 6, max: 15, message: "长度在6到15个字符", trigger: 'blur' },
+                ],
+            }
+        }
+    },
+    methods: {
+        resetLoginForm() {
+            this.$refs.loginFormRef.resetFields();
+        }
+    }
+}
 </script>
