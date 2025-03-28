38efd4203f250e21dfe5a8959ed7d69f34b28f02
xuzepei <xuzepei@gmail.com>
2025-03-28 15:00:42 +0800

    老项目折腾调通

--- my-vue-app/public/script.js
+++ my-vue-app/public/script.js
@@ -1,5 +1,3 @@
-
-
 function createGradientBg() {
   // 创建 Canvas 并绘制渐变
   const canvas = document.createElement('canvas');
@@ -11,6 +9,8 @@ function createGradientBg() {
   const gradient = context.createLinearGradient(0, 0, 0, canvas.height);
   gradient.addColorStop(0, '#010101'); // 黑色
   gradient.addColorStop(1, '#7575e8'); // 紫色
+//  gradient.addColorStop(0, '#F2F2F7');
+//  gradient.addColorStop(1, '#F2F2F7');
 
   // 将渐变应用到 Canvas
   context.fillStyle = gradient;
@@ -28,41 +28,48 @@ var animate = function () {
   renderer.render(scene, camera);
 };
 
-var loadModel = function () {
-  // Load MTL and OBJ files
-  var mtlLoader = new THREE.MTLLoader();
-  mtlLoader.setPath('./models/');
-  mtlLoader.load('face.obj.mtl', function (materials) {
+var loadModel = function (path, filename) {
 
-    console.log(materials);
+    var name = filename;
 
-    materials.preload();
+    // Load MTL and OBJ files
+    var mtlLoader = new THREE.MTLLoader();
+    mtlLoader.setPath(path);
+    mtlLoader.load(name + '.mtl', function (materials) {
 
-    var objLoader = new THREE.OBJLoader();
-    objLoader.setMaterials(materials);
-    objLoader.setPath('./models/');
-    objLoader.load('face.obj', function (object) {
+        console.log(materials);
 
-      // 遍历模型的所有子对象
-      object.traverse(function (child) {
-        if (child.isMesh) {
-          // 设置材质的side属性为THREE.DoubleSide
-          child.material.side = THREE.DoubleSide;
-        }
-      });
+        materials.preload();
 
-      scene.add(object);
+        var objLoader = new THREE.OBJLoader();
+        objLoader.setMaterials(materials);
+        objLoader.setPath(path);
+        objLoader.load(name + '.obj', function (object) {
 
-      animate();
-    },
-      function (xhr) {
-        console.log((xhr.loaded / xhr.total * 100) + '%  obj loaded');
-      },
-      function (error) {
-        console.error('An error occurred while loading the OBJ file', error);
-      });
-  });
+              // 遍历模型的所有子对象
+              object.traverse(function (child) {
+                if (child.isMesh) {
+                  // 设置材质的side属性为THREE.DoubleSide
+                  child.material.side = THREE.DoubleSide;
+                }
+              });
+
+            scene.add(object);
+
+            animate();
+        },
+            function (xhr) {
+                console.log((xhr.loaded / xhr.total * 100) + '%  obj loaded');
+            },
+            function (error) {
+                console.error('An error occurred while loading the OBJ file', error);
+            });
+    });
+
+    return "true";
 };
+
+
 
 // 创建场景、摄像机和渲染器
 var scene = new THREE.Scene();
@@ -70,6 +77,8 @@ var scene = new THREE.Scene();
 scene.background = createGradientBg();
 
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 2.3;
+
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -83,7 +92,6 @@ directionalLight.castShadow = true;
 directionalLight.position.set(10, 10, 10).normalize();
 scene.add(directionalLight);
 
-
 // 创建OrbitControls来控制模型旋转
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
 // 设置缩放限制
@@ -91,13 +99,10 @@ controls.minDistance = 1; // 最小距离
 controls.maxDistance = 10; // 最大距离
 // 如果OrbitControls改变了相机参数，重新调用渲染器渲染三维场景
 controls.addEventListener('change', function () {
-  renderer.render(scene, camera); //执行渲染操作
+    renderer.render(scene, camera); //执行渲染操作
 });//监听鼠标、键盘事件
 
 
-camera.position.z = 3;
-
 animate();
 
-loadModel();
-
+loadModel("models/", "20190618124505345_0_hrn_mid_mesh");
--- my-vue-app/src/main.js
+++ my-vue-app/src/main.js
@@ -1,6 +1,6 @@
 import {createApp } from 'vue'
 import App from './App.vue'
-import './plugins/element.js'
+//import './plugins/element.js'
 
 //Vue3.0 Filters are deprecated.
 //Vue.filter("reverse", (value, argument)=> { value.split("").reverse().join(argument); })
